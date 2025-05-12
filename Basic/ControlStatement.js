32696a85e37f465dec422db3ae25292160332b33
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-12 09:59:08 +0530

    String Methods

---
+++ Basic/ControlStatement.js
@@ -0,0 +1,88 @@
+console.log("Control statement");
+
+//  1. condition control
+    /*
+        1. if statement
+            if(condition){
+                code
+            }
+        2. if-else statement
+            if(condition){
+                code;
+            }else{
+                code;
+            }
+        3. else-if statement
+           if(condition){
+                code;
+            }else if(condition2){
+                code;
+            }else{
+                code;
+            }
+        4. nested statement
+            if(condition){
+                code;
+            }else{
+                   if(condition){
+                        code;
+                    }else{
+                        code;
+                    }
+            }
+    */
+
+//  2. case control
+    /*
+        1. switch case
+        
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+        2. nested switch case
+    */ 
+
+
+// var key = 13;
+// switch (key) {
+//     case 1: console.log("case 1");
+        
+//         break;
+//     case "Zafar" : console.log("case 2");
+//         break;
+//     case (12 && 13) : console.log("case 13");
+//             break;
+//     default:
+//         break;
+// }
+
+//  3. loop control 
+    /*
+        1. for loop
+    
+        2. while loop
+        3. do-while loop
+    */
+for (let i = 0; i < 10; i++) {
+
+    
+}
+
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
+        /*
+            More : 
+                1. foreach loop
+                2. for-in loop
+                3. for-of loop
+        */ 
---
+++ Most important/String/string1.js
@@ -0,0 +1,92 @@
+// in C : 
+/*
+    char str[] = ""
+    char str[] = {""}
+    char str[100];
+*/
+
+// var  str1=   "Royal";
+// let  str2 ="Royal";
+// const str3 ="Royal";
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// Double Quote : single Line String
+// var  str1 = "Royal";
+// Single Quote  : single Line String
+// var  str1 = 'Royal';
+// Backtick    :   multiline String
+// var str1 = `Royal`;
+// console.log(str1);
+
+// console.log("This is a New Line");
+// console.log('This is a New Line');
+// console.log(`This is 
+//     a New Line`);
+
+
+
+
+var mystr = 'Royal Technosoft pvt ltd';
+//index      01234.....................
+// console.log(mystr);
+
+// console.log(mystr[0]);
+// console.log(mystr[19]);
+// console.log(mystr[mystr.length - 1]);
+
+
+// property : 
+// 1. length  
+// console.log(mystr.length);
+
+
+// Methods : 
+
+// mystr.at
+// console.log(mystr.at(123));
+// mystr.charAt
+// console.log(mystr.charAt(123));
+
+// mystr.charCodeAt
+// console.log(mystr.charCodeAt(3));
+
+// mystr.concat
+// console.log(mystr.concat("asdfgh"));
+
+// mystr.startsWith
+// mystr.endsWith
+// console.log(mystr.startsWith('R'));
+// console.log(mystr.startsWith('Roya'));
+// console.log(mystr.startsWith('roya'));
+// console.log(mystr.startsWith('RoYa'));
+
+// console.log(mystr.endsWith('d'));
+// console.log(mystr.endsWith('ltd'));
+// console.log(mystr.endsWith('_ltd'));
+
+
+
+// mystr.includes
+// console.log(mystr.includes('al'));
+// console.log(mystr.includes('ay'));
+
+
+// ---------------------------------------------------------
+
+// mystr.indexOf
+// mystr.lastIndexOf
+
+// mystr.replace
+// mystr.replaceAll
+// mystr.slice
+// mystr.substring
+// mystr.substr
+
+// mystr.split
+// mystr.toLowerCase
+// mystr.toUpperCase
+// mystr.trim
+// mystr.trimEnd
+// mystr.trimStart
