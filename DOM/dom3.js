5046a1ac15ac0e3dcaa447dbfd411a46654a5a42
Shabhaizafar <zafarshabhai03@gmail.com>
2025-06-25 10:55:58 +0530

    password

--- DOM/dom3.js
+++ DOM/dom3.js
@@ -1,7 +1,6 @@
 var i = 0;
 document.body.onkeypress = function (event){
     i++;
-    // console.log(event.key);
     document.querySelector('div').innerHTML += event.key;
     document.querySelector('.tkey').innerHTML = i; 
 }
