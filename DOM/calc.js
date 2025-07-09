e1029971f5996cc39d365d6aea7c1d18c1101dbf
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-09 10:34:34 +0530

    Calc

--- DOM/calc.js
+++ DOM/calc.js
@@ -1,15 +1,39 @@
-document.querySelector('.buttons').onclick = function (event){
-    if(event.target.tagName == "BUTTON"){
-        if(event.target.classList.contains("equal")){
-            console.log(document.querySelector('.display').innerText);
+var arr = ["+", '-'];
+
+function calculate(temp,n1,n2) {
+    switch (temp) {
+        case "+": return +n1 + +n2;
+        case "-": return +n1 - +n2;
+        case "*": return +n1 * +n2;
+    }
+}
+document.querySelector('.buttons').onclick = function (event) {
+    if (event.target.tagName == "BUTTON") {
+        if (event.target.classList.contains("equal")) {
+            var str = document.querySelector('.display').innerText;
+            var j = 0;
+            var ans = 0;
+            var temp = "+";
+            for (let i = 0; i < str.length; i++) {
+                if (i == str.length - 1) {
+                    ans = calculate(temp,ans,str.slice(j));
+                }
+                if (arr.includes(str[i])) {
+                    ans = calculate(temp,ans,str.slice(j, i));
+                    temp = str[i];
+                    j = i + 1;
+                }
+            }
+            document.querySelector('.final').innerHTML = str + "=";
+            document.querySelector('.display').innerText = ans;
             return;
         }
         // if(event.target.classList.contains('operator')){
         //     // 
         // }
-        if(document.querySelector('.display').innerText=="0"){
+        if (document.querySelector('.display').innerText == "0") {
             document.querySelector('.display').innerText = event.target.innerText;
-        }else{
+        } else {
             document.querySelector('.display').innerText += event.target.innerText;
         }
     }
